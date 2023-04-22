805d1328eb3b74840fe7892c0a5ca0d0ba9b8d91
Jolin <rca8vx@virginia.edu>
2023-04-22 01:09:33 -0400

    saving changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
+import ProgressBar from '../components/ProgressBar'
 
 function Home() {
   return (
     <div>
       <Sidebar />
       <Header />
+      <ProgressBar />
       Home
     </div>
   )
